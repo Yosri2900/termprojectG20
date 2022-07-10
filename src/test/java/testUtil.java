6d4f8b323f875904b4eeedf7bce8f8346c9cb5ce
Mohanad Mohamed <mmoha323@uottawa.ca>
2022-07-10 13:37:46 -0400

    added remaining test cases for Util.java

--- src/test/java/testUtil.java
+++ src/test/java/testUtil.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
+import static org.junit.Assert.*;
 
 public class testUtil {
 
@@ -65,5 +66,59 @@ public class testUtil {
   public void testIsArrayNumeric04() {
     String[] content = new String[]{"1", "2", "3", "4", "5"};
     assertTrue(Util.isArrayNumeric(content));
+  }
+  
+  
+  @Test
+  public void testnominalArrayToString01() {
+    String[] content = new String[]{"a", "b", "c", "d", "e"};
+    assertEquals("['a', 'b', 'c', 'd', 'e']", Util.nominalArrayToString(content));
+  }
+  
+  @Test
+  public void testnominalArrayToString02() {
+    String[] content = new String[]{"boom", "Hello", "World", "$qmwd12@!#%", ""};
+    assertEquals("['boom', 'Hello', 'World', '$qmwd12@!#%', '']", Util.nominalArrayToString(content));
+  }
+  
+  @Test
+  public void testnominalArrayToString03() {
+    assertEquals(null, Util.nominalArrayToString(null));
+  }
+  
+  @Test
+  public void testnominalArrayToString04() {
+    String[] content = new String[]{""};
+    assertEquals("['']", Util.nominalArrayToString(content));
+  }
+  
+  @Test
+  public void testnominalArrayToString05() {
+    String[] content = new String[]{"word"};
+    assertEquals("['word']", Util.nominalArrayToString(content));
+  }
+  
+  @Test
+  public void testnumericArrayToString01() {
+	Util obj = new Util();
+    String[] content = new String[]{"12", "100", "", "32", "0"};
+    assertEquals("[12, 100, , 32, 0]", obj.numericArrayToString(content));
+  }
+  
+  @Test
+  public void testnumericArrayToString02() {
+    String[] content = new String[]{"1"};
+    assertEquals("[1]", Util.numericArrayToString(content));
+  }
+  
+  @Test
+  public void testnumericArrayToString03() {
+    String[] content = new String[]{""};
+    assertEquals("[]", Util.numericArrayToString(content));
+  }
+  
+  @Test
+  public void testnumericArrayToString04() {
+    assertEquals(null, Util.numericArrayToString(null));
   }
 }
